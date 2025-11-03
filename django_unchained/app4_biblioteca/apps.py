1436a68cd8e69d7de683667aee48389d3e87286f
m88perezt <m88perezt@gmail.com>
2025-11-03 17:35:43 +0100

    corregido nombre

--- django_unchained/app4_biblioteca/apps.py
+++ django_unchained/app4_biblioteca/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AppBibliotecaConfig(AppConfig):
+class App4BibliotecaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_biblioteca'
+    name = 'app4_biblioteca'
